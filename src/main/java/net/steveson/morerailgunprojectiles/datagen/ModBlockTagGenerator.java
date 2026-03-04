5f14f9596cdc4b4669068c6b8f324e2e089af4f5
Steve-Steveson <mr.steve.steveson@gmail.com>
2026-03-03 21:45:59 -0500

    test datagen copy over pt 1

--- src/main/java/net/steveson/morerailgunprojectiles/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/steveson/morerailgunprojectiles/datagen/ModBlockTagGenerator.java
@@ -1,4 +1,22 @@
 package net.steveson.morerailgunprojectiles.datagen;
 
-public class ModBlockTagGenerator {
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.steveson.morerailgunprojectiles.MoreRailgunProjectiles;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagGenerator extends BlockTagsProvider {
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, MoreRailgunProjectiles.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+
+
+    }
 }
