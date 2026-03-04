7a3da505083609836089eaa5c24d3f9b26bc4513
Steve-Steveson <mr.steve.steveson@gmail.com>
2026-03-03 21:35:40 -0500

    Attempt to call tags not yet working. Some temp test code added, more to come.

---
+++ src/main/java/net/steveson/morerailgunprojectiles/api/MRPTags.java
@@ -0,0 +1,41 @@
+package net.steveson.morerailgunprojectiles.api;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class MRPTags {
+//    public static class Blocks{
+//
+//    }
+//
+//    public static class Items{
+//
+//
+//    }
+
+    private MRPTags() {
+
+    }
+
+    public static final TagKey<Item> RODS_CONSTANTAN = commonTag("rods/constantan");
+    public static final TagKey<Item> RODS_BRONZE = commonTag("rods/bronze");
+
+    public static final TagKey<Item> RODS_GOLD = commonTag("rods/gold");
+
+
+    private static TagKey<Item> commonTag(String name) {
+
+        return ItemTags.create(new ResourceLocation("c", name));
+    }
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/net/steveson/morerailgunprojectiles/item/AddonRailgunProjectiles.java
+++ src/main/java/net/steveson/morerailgunprojectiles/item/AddonRailgunProjectiles.java
@@ -1,7 +1,9 @@
 package net.steveson.morerailgunprojectiles.item;
 
+import blusunrize.immersiveengineering.api.IETags;
 import blusunrize.immersiveengineering.api.tool.RailgunHandler;
 import net.minecraftforge.common.Tags;
+import net.steveson.morerailgunprojectiles.api.MRPTags;
 
 public class AddonRailgunProjectiles {
 
@@ -12,7 +14,25 @@ public class AddonRailgunProjectiles {
                 new RailgunHandler.RailgunRenderColors(0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff)
         );
 
+        // Constantan
+        RailgunHandler.registerStandardProjectile(MRPTags.RODS_CONSTANTAN, 8, 1.25).setColorMap(
+                new RailgunHandler.RailgunRenderColors(0xff0000, 0xff0000, 0xff0000, 0xffff00, 0xffff00, 0xff0000)
+        );
 
+        // Graphite ingot test
+        RailgunHandler.registerStandardProjectile(IETags.hopGraphiteIngot, 8, 1.25).setColorMap(
+                new RailgunHandler.RailgunRenderColors(0xff0000, 0xff0000, 0xff0000, 0xffff00, 0xffff00, 0xff0000)
+        );
+
+        // Gold
+        RailgunHandler.registerStandardProjectile(MRPTags.RODS_GOLD, 4, 1.25).setColorMap(
+                new RailgunHandler.RailgunRenderColors(0xffff00, 0xffff00, 0xffff00, 0xffff00, 0xffff00, 0xffff00)
+        );
+
+        // Bronze
+        RailgunHandler.registerStandardProjectile(MRPTags.RODS_BRONZE, 8, 1.25).setColorMap(
+                new RailgunHandler.RailgunRenderColors(0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff)
+        );
 
 
     }
